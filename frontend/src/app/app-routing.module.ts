5b7b67477db78a2715bf89efa2eae55f7c84c527
luismigeek <luismigeek@gmail.com>
2020-04-26 11:55:39 -0500

    [Angular Routes]: se establecieron rutas para los componentes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './componentes/login/login.component';
+import { SingupComponent } from './componentes/singup/singup.component';
+import { RequestResetComponent } from './componentes/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './componentes/password/response-reset/response-reset.component';
+import { ProfileComponent } from './componentes/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'singup', component: SingupComponent},
+  {path: 'password-request-reset', component: RequestResetComponent},
+  {path: 'password-response-reset', component: ResponseResetComponent},
+  {path: 'profile', component: ProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
