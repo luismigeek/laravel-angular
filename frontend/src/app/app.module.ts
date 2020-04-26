aa7b0727cd153be0917d5ad07dd313bfee4c1868
luismigeek <luismigeek@gmail.com>
2020-04-26 10:44:35 -0500

    [Creación de componentes]: Se crearon los componentes necesarios para la autenticación

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { SingupComponent } from './componentes/singup/singup.component';
+import { ProfileComponent } from './componentes/profile/profile.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { RequestResetComponent } from './componentes/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './componentes/password/response-reset/response-reset.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SingupComponent,
+    ProfileComponent,
+    NavbarComponent,
+    RequestResetComponent,
+    ResponseResetComponent
   ],
   imports: [
     BrowserModule,
