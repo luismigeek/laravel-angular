b6f94b33c79e546758d5300aaa7862608e113ca7
luismigeek <luismigeek@gmail.com>
2020-05-01 01:57:03 -0500

    [Controlador para enviar email]: Se añadió un nuevo controlador para envio de email

---
+++ backend/app/Http/Controllers/PasswordResetController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ResetPasswordMail;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class PasswordResetController extends Controller
+{
+    public function sendEmail(Request $request)
+    {
+        if (!$this->validarCorreo($request->email)) {
+            return $this->failedResponse();
+        }
+
+        $this->send($request->email);
+        return $this->successResponse();
+    }
+
+    public function send($email)
+    {
+        // $token = $this->createToken($email);
+        Mail::to($email)->send(new ResetPasswordMail);
+    }
+
+    public function createToken($email)
+    {
+        $oldToken = DB::table('password_resets')->where('email', $email)->first();
+
+        if ($oldToken) {
+            return $oldToken->token;
+        }
+
+        $token = str_random(60);
+        $this->saveToken($token, $email);
+        return $token;
+    }
+
+    public function saveToken($token, $email)
+    {
+        DB::table('password_resets')->insert([
+            'email' => $email,
+            'token' => $token,
+            'created_at' => Carbon::now()
+        ]);
+    }
+
+
+    public function validarCorreo($correo)
+    {
+        return !!User::where('email',$correo)->first();
+    }
+
+    public function validateEmail($email)
+    {
+        return !!User::where('email', $email)->first();
+    }
+
+    public function failedResponse()
+    {
+        return response()->json([
+            'error' => 'No se encontró el correo en la base de datos'
+        ], Response::HTTP_NOT_FOUND);
+    }
+
+    public function successResponse()
+    {
+        return response()->json([
+            'data' => 'Correo de restablecimiento de contraseña fue enviado exitosamente, por favor revise su bandeja de entrada.'
+        ], Response::HTTP_OK);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,4 +8,5 @@ Route::group([
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');    
+    Route::post('sendPasswordResetLink', 'PasswordResetController@sendEmail');
 });
